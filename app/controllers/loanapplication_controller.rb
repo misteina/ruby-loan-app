9ea137761f651c9bedb4dfb87b4d5ffa5ca232a4
Enyinna <enyinna.tech@gmail.com>
2020-08-11 20:42:06 +0100

    11-8-2020 20:42

--- app/controllers/loanapplication_controller.rb
+++ app/controllers/loanapplication_controller.rb
@@ -1,5 +1,41 @@
 class LoanapplicationController < ApplicationController
     def action
-        
+        if cookie.signed[:tis]
+            require 'date'
+
+            amount = params[:amount].to_i
+            duration = params[:duration]
+
+            errors = []
+            dateFormat = Date.strptime(duration, '%d-%m-%Y') rescue false
+            allowFigures = [2500, 5000, 10000, 20000, 40000, 80000, 100000, 120000, 150000]
+
+            if !allowFigures.include? amount
+                errors.push('Invalid amount')
+            end
+            if !dateFormat
+                errors.push('Invalid loan tenure date')
+            end
+
+            if errors.length == 0
+                duration = Date.parse("20120225").strftime("%Y-%m-%d")
+                userId = cookie.signed[:tis]
+
+                query = 'INSERT INTO employment (uid, amount, dueDate) VALUES (?, ?, ?)'
+                dataArray = [query, userId, amount, duration]
+                sanitizedQuery = ActiveRecord::Base.send(:sanitize_sql_array, dataArray)
+                result = ActiveRecord::Base.connection.update(sanitizedQuery)
+
+                if result == 1
+                    render json: {success: 'done'}
+                else
+                    render json: {error: 'failed'}
+                end
+            else
+                render json: {error: errors}
+            end
+        else
+            render json: {error: 'Not authorized'}
+        end
     end
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,16 +1,36 @@
 class LoginController < ApplicationController
-    skip_before_action :require_login
-    before_action :check_login
-
     def action
+        if cookie.signed[:tis]
 
-    end
+            phone = params[:phone]
+            password = param[:password]
 
-    private
-    def check_login
-        if  cookies.signed[:wdt]
-            @error = Hash["error" => "Illegal"]
-            render json: @error
+            errors = []
+            if phone.length != 11
+                errors.push('Invalid phone number')
+            end
+            if password.length > 50 || password.length < 3
+                errors.push('Invalid password')
+            end
+
+            if errors.length == 0
+                query = 'SELECT password FROM register WHERE phone = ?'
+                dataArray = [query, phone]
+                sanitizedQuery = ActiveRecord::Base.send(:sanitize_sql_array, dataArray)
+                result = ActiveRecord::Base.connection.select_one(sanitizedQuery)
+
+                if result.length == 1
+                    if BCrypt::Password.new(result[:password]) == password
+                        render json: {success: 'done'}
+                    else
+                        render json: {error: 'Incorrect login credentials'}
+                    end
+                else
+                    render json: {error: 'Not registered'}
+                end
+            else
+                render json: {error: errors}
+            end
         end
     end
 end
